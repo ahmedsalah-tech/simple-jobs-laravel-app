ae796b01ca285bea5787a59f017235dd1eddf602
Ahmed Salah <219439781+ahmedsalah-tech@users.noreply.github.com>
2026-01-06 18:01:36 -0800

    Add WorkSeeder to DatabaseSeeder for populating Work data

--- database/seeders/WorkSeeder.php
+++ database/seeders/WorkSeeder.php
@@ -3,15 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Work;
 use Illuminate\Database\Seeder;
 
-class WorkSeeder extends Seeder
+class WorkSeeder extends Seeder  // used to pipulate the data using it's factory on db change
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Work::factory()->count(50)->create();
     }
 }

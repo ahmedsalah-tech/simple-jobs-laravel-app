68e3e3b54d614564ed5208174ab327ccf4751f97
Ahmed Salah <219439781+ahmedsalah-tech@users.noreply.github.com>
2026-01-14 19:10:30 -0800

    Add validation error display to registration form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,23 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function showRegister() {
+    public function showRegister () {
         return view('auth.register');
     }
 
-    public function showLogin() {
+    public function showLogin () {
         return view('auth.login');
     }
 
-     public function register() {
+     public function register(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|max:64|confirmed' // hashed automatically in newer versions of Laravel
+        ]);
 
+        $user = User::create($validated);
+
+        Auth::login($user);
+
+        return redirect()->route('jobs.index');
     }
 
-    public function login() {
-        
+    public function login () {
+
     }
 }

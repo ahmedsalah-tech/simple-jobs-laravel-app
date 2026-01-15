d129fe2eedcfc70f60d07658c66ecc7fc21eb45a
Ahmed Salah <219439781+ahmedsalah-tech@users.noreply.github.com>
2026-01-14 20:39:12 -0800

    Refactor authentication routes to use middleware for guest and auth users

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
-Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
-// Named Route to be dynamically called
-Route::get('/jobs/{job}',  [JobController::class, 'show'])->name('jobs.show'); // Route model binding
-Route::post('/jobs',  [JobController::class, 'store'])->name('jobs.store');
-Route::delete('/jobs/{job}',  [JobController::class, 'destroy'])->name('jobs.destroy');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('/register',  'showRegister')->name('show.register');
+    Route::get('/login',  'showLogin')->name('show.login');
+    Route::post('/register',  'register')->name('register');
+    Route::post('/login',  'login')->name('login');
+});
+
+
+// Controller function assigms a fully qualified controller to the routes
+Route::middleware('auth')->controller(JobController::class)->group(function () {
+    Route::get('/jobs',  'index')->name('jobs.index');
+    // built-in single-route middlware in laravel to intercept unauthenticated urequests and forwards them to the login route (->middleware('auth');)
+    Route::get('/jobs/create',  'create')->name('jobs.create');
+    // Named Route to be dynamically called
+    Route::get('/jobs/{job}',   'show')->name('jobs.show'); // Route model binding
+    Route::post('/jobs',   'store')->name('jobs.store');
+    Route::delete('/jobs/{job}',   'destroy')->name('jobs.destroy');
+});
